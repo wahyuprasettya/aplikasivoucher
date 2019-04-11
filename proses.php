ae2261550e4177d24862b0dc39daaa5326e33415
wahyu adjie prasettyo <32014966+wahyuprasettya@users.noreply.github.com>
2019-04-11 21:33:26 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -12,18 +12,17 @@ if (isset($_POST['proses'])) {
 
 
 if ($kode== "") {
-		echo "Total bayar = "; echo $jumlbyar;
-		$_SESSION['tanpadis'] = $jumlbyar; 
+echo "Total bayar = "; echo $jumlbyar;
+	$_SESSION['tanpadis'] = $jumlbyar; 
 		header("location: hasiltanpadiskon.php");
 }else if ($kv != $kode) {
-
-	 header("location:form-basic.php?kodevoucher=salah");
+	header("location:form-basic.php?kodevoucher=salah");
 }elseif ($jumlbyar <= 500) {
 	 header("location:form-basic.php?harga=salah");
 }else {
-	$hasil= $jumlbyar - 500;
+$hasil= $jumlbyar - 500;
 	$_SESSION['jumlahbyr'] = $hasil; 
-	header("location: hasildiskon.php");
+		header("location: hasildiskon.php");
 }
 
 }
